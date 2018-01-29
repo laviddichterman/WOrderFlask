2aae11c39bb1ea34cc15c2c02e76a5a747a59a24
Dave Lichterman <me@lavid.me>
2018-01-29 21:50:00 +0000

    More scaffolding

--- resources/order.py
+++ resources/order.py
@@ -1,14 +1,42 @@
 from flask_restful import Resource, reqparse, fields, marshal_with
 
 
-order_fields = {
-    'o_id':         fields.String,
-    'products':     fields.List,
-    'status':       fields.String,
-}
+class WClientData(object):
+    """Client metadata used for logging
+    """
+    marshal_fields = {
+        'load_time':    fields.DateTime,
+        'submit_time':  fields.DateTime,
+        'time_selection_time':    fields.DateTime,
+        'user_agent':    fields.String
+    }
+
+    def __init__(self,
+                 load_time,
+                 submit_time,
+                 time_selection_time,
+                 user_agent):
+        self.load_time = load_time
+        self.submit_time = submit_time
+        self.time_selection_time = time_selection_time
+        self.user_agent = user_agent
+
+
+class WProduct(object):
+    """A Product: a good or a service
+    """
+    marshal_fields = {
+    }
 
 
 class WOrder(object):
+    marshal_fields = {
+        'o_id':         fields.String,
+        'products':     fields.List(fields.String),
+        'status':       fields.String,
+        'client_data':  fields.Nested(WClientData.marshal_fields)
+    }
+
     def __init__(self,
                  o_id,
                  products,
@@ -37,13 +65,13 @@ class OrderById(Resource):
         # self.db = kwargs['db']
         Resource.__init__(self)
 
-    @marshal_with(order_fields)
+    @marshal_with(WOrder.marshal_fields)
     def get(self, o_id):
         return orders[o_id]
 
 
 class OrderPost(Resource):
-    @marshal_with(order_fields)
+    @marshal_with(WOrder.marshal_fields)
     def post(self):
         args = parser.parse_args()
         order_id = str(int(max(orders.keys())) + 1)
