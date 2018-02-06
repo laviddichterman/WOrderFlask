6bb9d8359b14282b28bbb5c2f8af93b536631b52
Dave Lichterman <me@lavid.me>
2018-02-06 21:45:04 +0000

    More order scaffolding

--- resources/order.py
+++ resources/order.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse, fields, marshal_with
+import enum
 
 
 class WClientData(object):
@@ -67,9 +68,58 @@ class WOption(WProduct):
         self.enable_function_name = enable_function_name
 
 
+class WToppingPlacement(enum.Enum):
+    NONE = 0
+    LEFT = 1
+    RIGHT = 2
+    WHOLE = 3
+
+
+class WTopping(WOption):
+    marshal_fields = WOption.marshal_fields.copy()
+    marshal_fields.update({'flavor_factor': fields.Float})
+    marshal_fields.update({'bake_factor': fields.Float})
+    marshal_fields.update({'idx': fields.Integer})
+
+    def __init__(self, idx, flavor_factor, bake_factor, *args, **kwargs):
+        super(WTopping, self).__init__(self, args, kwargs)
+        self.idx = idx
+        self.flavor_factor = flavor_factor
+        self.bake_factor = bake_factor
+
+
+class WPlacedTopping(object):
+    """A tuple of a @WTopping and a @WToppingPlacement
+    """
+    marshal_fields = {
+        'topping': fields.Nested(WTopping.marshal_fields),
+        'placement': fields.String
+    }
+
+    def __init__(self, placement, topping):
+        self.placement = placement
+        self.topping = topping
+
+
+class WPizza(WProduct):
+    marshal_fields = WProduct.marshal_fields.copy()
+    marshal_fields.update({
+        'toppings': fields.Nested(WPlacedTopping.marshal_fields)})
+    marshal_fields.update({
+        'crust_flavor': fields.Nested(WOption.marshal_fields)})
+    marshal_fields.update({
+        'dough': fields.Nested(WOption.marshal_fields)})
+    marshal_fields.update({
+        'cheese': fields.Nested(WOption.marshal_fields)})
+    marshal_fields.update({
+        'sauce': fields.Nested(WOption.marshal_fields)})
+
+
 class WOrder(object):
     marshal_fields = {
         'o_id': fields.String,
+        # TODO: smart polymorphic compatible way to get the right polymorphic
+        # nested fields thingy
         'products': fields.List(fields.Nested(WProductQuantity.marshal_fields)),
         'status': fields.String,
         'client_data': fields.Nested(WClientData.marshal_fields)
